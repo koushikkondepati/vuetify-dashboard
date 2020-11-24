e5fbdb487132211dd046e7a8c617ea8e62ed0862
koushikdev1 <koushik@billionlives.in>
2020-11-24 06:31:52 +0530

    router added, working properly

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,28 @@
 <template>
  <v-app>
-  <v-app-bar app color="primary" dark>
+  <v-app-bar app class="teal darken-4" dark >
      <v-toolbar-title>
        Vuetify Dashboard
      </v-toolbar-title>
      <v-spacer />
-     <v-btn text rounded>HOME</v-btn>
-     <v-btn text rounded>Login</v-btn>
+     <v-btn 
+      v-for="link in links"
+      :key="`${link.label}-header-link`"
+      text 
+      rounded 
+      :to="link.url"
+    >
+     {{link.label}}
+    </v-btn>
+     
    </v-app-bar>
-  <v-content>
+  <v-content >
     <!-- login module -->
-    <v-card width="400" class="mx-auto mt-5">
-        <v-card-title>
-          <h1 class="display-1">Login</h1>
-        </v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field label="Username" prepend-icon="mdi-account-circle" />
-            <v-text-field 
-              :type="showPassword ? 'text' : 'password'" 
-              label="Password"  
-              prepend-icon="mdi-lock" 
-              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-              @click:append="showPassword = !showPassword" 
-            />
-          </v-form>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn color="success">Register</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn color="info">Login</v-btn>
-        </v-card-actions>
-        
-    </v-card>
+    <router-view></router-view>
   </v-content>
   <!-- footer -->
   <v-footer
-    color="primary lighten-1"
+    color="teal darken-2"
     padless
   >
     <v-row
@@ -45,16 +31,17 @@
     >
       <v-btn
         v-for="link in links"
-        :key="link"
+        :key="`${link.label}-footer-link`"
         color="white"
         text
         rounded
         class="my-2"
+        :to="link.url"
       >
-        {{ link }}
+        {{ link.label }}
       </v-btn>
       <v-col
-        class="primary lighten-2 py-4 text-center white--text"
+        class="teal darken-4 py-4 text-center white--text"
         cols="12"
       >
         {{ new Date().getFullYear() }} — <strong>Vuetify Dashboard</strong>
@@ -74,10 +61,16 @@ export default {
     
   },
   data: () => ({
-    showPassword: false,
+    
     links: [
-        'Home',
-        'About Us'
+        {
+          label: 'Home',
+          url: '/'
+        },
+        {
+          label: 'Login',
+          url: '/login'
+        }
       ]
   }),
 }
